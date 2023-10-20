3a4333a694cee49076e19658de841ab1d0be4915
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-10-21 01:39:48 +0530

    Add default pages and components
    Added some pages and components which are required in any project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,32 @@
 import("preline");
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 };
 
 export default App;
