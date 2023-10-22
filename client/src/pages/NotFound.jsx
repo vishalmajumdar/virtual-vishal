bebf68208710c484ddba41372c8ed4adf8a19c9b
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-10-22 12:22:46 +0530

    Add 404 sections

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import CommonHero from "../components/CommonHero";
+import Divider from "../components/Divider";
+
 const NotFound = () => {
-  return <>Not Found</>;
+  return (
+    <>
+      <CommonHero
+        heading="Oops, Something's Missing"
+        brief="We're here to help you find your way. If you have questions, need assistance, or simply want to chat, please don't hesitate to reach out. We're committed to guiding you on your journey."
+        cta="Contact Us"
+      />
+      <Divider />
+    </>
+  );
 };
 
 export default NotFound;
